05b02340db515eb4c8c3f6e1c77d21ed9a0d7a24
Assma Ben Aicha <benaicha@polytech.unice.fr>
2014-06-09 23:52:47 +0200

    liaison de l'activité qui gère les activité récurante avec celle des paarametres initiles

---
+++ app/src/main/java/fr/unice/polytech/freetime/app/RecurenteEvent.java
@@ -0,0 +1,51 @@
+package fr.unice.polytech.freetime.app;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class RecurenteEvent extends ActionBarActivity {
+
+    private User user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recurente_event);
+        user = (User) getIntent().getSerializableExtra("userParam");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.recurente_event, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick_okValid(View view){
+        user.setSetDailiesAct(true);
+        int step=user.getStep();
+        user.setStep(++step);
+        Intent intent= new Intent(this, FirstParam2.class);
+        intent.putExtra("user",user);
+        startActivity(intent);
+
+    }
+}
