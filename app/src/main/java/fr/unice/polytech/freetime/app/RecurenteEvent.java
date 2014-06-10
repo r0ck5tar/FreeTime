a326be4b909928d55761b923c64ade7214c9917c
Assma Ben Aicha <benaicha@polytech.unice.fr>
2014-06-10 08:57:09 +0200

    e en placed'un datePicker

--- app/src/main/java/fr/unice/polytech/freetime/app/RecurenteEvent.java
+++ app/src/main/java/fr/unice/polytech/freetime/app/RecurenteEvent.java
@@ -6,16 +6,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 
 
 public class RecurenteEvent extends ActionBarActivity {
 
     private User user;
+    //private DatePicker
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recurente_event);
         user = (User) getIntent().getSerializableExtra("userParam");
+
+
     }
 
 
